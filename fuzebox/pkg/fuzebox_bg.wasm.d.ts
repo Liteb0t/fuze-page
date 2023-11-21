29b89cb67d63e08dddd78f1f88066e3d5b12b562
tricerasmash <mpandccfanclub@gmail.com>
2023-11-21 21:33:01 +0000

    remove that sneaky gitignore

---
+++ fuzebox/pkg/fuzebox_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function show_code(a: number, b: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
